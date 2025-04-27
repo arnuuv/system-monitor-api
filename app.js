76c9934f41462d31e6626b53fb71c64b6bda2280
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-04-27 20:33:00 +0100

    requirements and goals

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const os = require('os');
+const process = require('process');
+const url = require('url');
+
+//!Format bytes to human readable
+//?Format seconds to human readable
+//Get CPU info
+//Get memory info
+//Get os info
+//Get network info
+//Get process
+//! Http Server
+//!Start a server
