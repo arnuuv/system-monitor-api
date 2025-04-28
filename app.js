d9721d453f3446aca2bb7173124dad70cf6c6369
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-04-28 12:31:25 +0100

    get process and create server

--- app.js
+++ app.js
@@ -82,5 +82,19 @@ function getSystemUptime(){
 
 
 //Get process
+function getProcess(){
+  const process = process.info()
+  console.log(process)
+}
+
+
+
 //! Http Server
+const server = http.createServer((req,res)=>{
+  res.writeHead(200,{'Content-Type':'text/html'})
+  res.end('<h1>Hello World</h1>')
+})
+server.listen(3000,'localhost',()=>{
+  console.log('Server is running on port 3000')
+})
 //!Start a server
