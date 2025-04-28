7bca29933758df3b9dc0b4217ae1e354da1e1af7
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-04-28 12:21:43 +0100

    format bytes

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ const process = require('process');
 const url = require('url');
 
 //!Format bytes to human readable
-//?Format seconds to human readable
+function formatBytes(bytes,decimal = 2) {
+    if (bytes ==0) return '0 bytes'
+    //set base uni
+    const k = 1024;
+    const sizes = ["Bytes","KB","MB","GB","TB","PB"];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(decimal)) + ' ' + sizes[i];
+  }
+
+
+
+
+
+
+
+  //?Format seconds to human readable
 //Get CPU info
 //Get memory info
 //Get os info
